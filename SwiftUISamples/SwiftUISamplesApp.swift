ae9dd914284a9ca9b9bd941b4016a7b2e80fdf5d
Alexandra Kostrova <sashabravos@gmail.com>
2023-08-07 23:41:07 +0300

    Add color exstension, background gradient and fisrt test of animation

---
+++ SwiftUISamples/SwiftUISamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISamplesApp.swift
+//  SwiftUISamples
+//
+//  Created by Александра Кострова on 07.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
